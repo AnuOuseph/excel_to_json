c81a723d8bb4aa7048a2391ff97a7b23c5280c6f
AnuOuseph <anuouseph04@gmail.com>
2024-01-02 12:14:28 +0530

    Bulk upload completed

--- server/controllers/excelController.js
+++ server/controllers/excelController.js
@@ -6,36 +6,30 @@ const uploadExcel = async (req, res) => {
   const excelBuffer = req.file.buffer;
 
   try {
+
     await workbook.xlsx.load(excelBuffer);
+    workbook.eachSheet(async (worksheet) => {
 
-    const allStudentsData = [];
-
-    workbook.eachSheet((worksheet) => {
       const headers = worksheet.getRow(1).values;
-      console.log("herrere",headers)
+      worksheet.eachRow(async (row, rowNumber) => {
 
-      worksheet.eachRow( async(row, rowNumber) => {
-        // Skip the header row
         if (rowNumber === 1) {
           return;
         }
 
         const studentData = {};
-
         headers.forEach((header, colIndex) => {
+
           if (colIndex === 0) {
             return;
           }
-        
-          const cellValue = row.getCell(colIndex).value;
 
-          // Map headers to corresponding fields in the Mongoose schema
+          const cellValue = row.getCell(colIndex).value;
           switch (header) {
             case 'name':
               studentData.name = cellValue;
               break;
             case 'number':
-              // Check if the value is a valid number, otherwise treat it as a string
               studentData.number = !isNaN(cellValue) ? parseFloat(cellValue) : cellValue;
               break;
             case 'qualification':
@@ -53,35 +47,26 @@ const uploadExcel = async (req, res) => {
             case 'status':
               studentData.status = cellValue;
               break;
-            // Add more cases as needed for additional fields
             default:
-              // If the header doesn't match any field, consider it as a custom field
               studentData[header.toLowerCase()] = cellValue;
               break;
           }
         });
 
-        // if (!studentData.name || !studentData.number || !studentData.email) {
-        //   console.log(`Skipping row ${rowNumber} as name, number, or email is empty.`);
-        //   return;
-        // }
-        // console.log(`Processing row ${rowNumber}. Data:`, studentData)
-          const existingStudent = await ExcelData.findOne({ $or: [{ number: studentData.number }, { email: studentData.email }] });
-          // If no existing student found, insert the data into the database
+        if (studentData.name && studentData.number) {
+          const existingStudent = await ExcelData.findOne({ $or: [{ number: studentData.number }, { name: studentData.name }] });
           if (!existingStudent) {
             console.log(`Inserting data into the database. Data:`, studentData);
-            allStudentsData.push(studentData);
+            await ExcelData.create(studentData);
           } else {
-            console.log(`Skipping data with number ${studentData.number} and email ${studentData.email} as it already exists in the database.`);
+            console.log(`Skipping data with number ${studentData.number} and email ${studentData.name} as it already exists in the database.`);
           }
-
+        }else{
+          console.log(`Skipping row ${rowNumber} as name, number, or email is empty.`);
+          return;
+        }
       });
     });
-    
-    // Save all students' data to the database
-    console.log('Attempting to insert data into MongoDB:', allStudentsData);
-    await ExcelData.insertMany(allStudentsData);
-    console.log('Data successfully inserted into MongoDB');
 
     res.json({ message: 'Data successfully uploaded to the database.' });
   } catch (error) {
@@ -90,50 +75,6 @@ const uploadExcel = async (req, res) => {
   }
 };
 
-
-// const uploadExcel = async (req, res) => {
-//   const workbook = new exceljs.Workbook();
-//   const excelBuffer = req.file.buffer;
-
-//   workbook.xlsx.load(excelBuffer).then(async (workbook) => {
-//     try {
-//       for (const worksheet of workbook.worksheets) {
-//         const sheetData = [];
-
-//         worksheet.eachRow((row) => {
-//           // Assuming the first row contains headers, and you want to skip it
-//           if (row.number === 1) {
-//             return;
-//           }
-
-//           const studentData = {
-//             // Map your student data fields here based on the column order
-//             No: row.getCell(1).value,
-//             Name: row.getCell(2).value,
-//             Number: row.getCell(3).value,
-//             Education: row.getCell(4).value,
-//             Email: row.getCell(5).value,
-//             Country: row.getCell(6).value,
-//             Contacted: row.getCell(7).value,
-//             Status: row.getCell(8).value,
-//             Comment: row.getCell(9).value,
-//             // Add other fields as needed
-//           };
-
-//           sheetData.push(studentData);
-//         });
-
-//         // Save each student to the database
-//         await ExcelData.insertMany(sheetData);
-//       }
-
-//       res.json({ message: 'Data successfully uploaded to the database.' });
-//     } catch (error) {
-//       console.error('Error saving to MongoDB:', error);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//     }
-//   });
-// };
 
 const getDetails = async (req,res)=>{
     try {
